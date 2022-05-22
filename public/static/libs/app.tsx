2e22dcc829882cdc45325b8f9dfb5e24da05b69d
Arthur <Artieli007@gmail.com>
2022-05-22 11:09:58 -0700

    Stylize website with hobbyist theme. Add new overview picture!

--- public/static/libs/app.tsx
+++ public/static/libs/app.tsx
@@ -23,6 +23,63 @@ export class App extends React.Component {
   render() {
     return (
       <div>
+
+        <div id="content">
+          <div id="contentInner">
+            <div id="dummyHeader">
+              <div id="dummyHeaderInner">
+                <div id="dummyTitleOuter">
+                  <div id="dummyTitle"><i><b>Hobby Automation</b></i></div>
+                  <div id="dummySubtitle"><i><b>AI Augmented Software</b></i></div>
+                </div>
+                <div id="dummyContentLinks">
+                  Overview | Home Automation | AI Assistant | Cloud AI Server | Contact
+                </div>
+              </div>
+
+              <div id="dummyHeaderStrip">
+
+              </div>
+            </div>
+
+            <div id="contentPages">
+
+              <div id="overview">
+
+                <div id="overviewTitle">
+                  <b>Home Automation and AI Assistant Suite</b>
+                </div>
+                <div id="overviewSubtitle">
+                  Complete online guide
+                </div>
+
+                <div id="overviewImage">
+                  <img id="overviewImageImg" src={require("../../../assets/overview.png").default}/>
+                </div>
+
+                CONTENT HERE.
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+        <div id="header">
+          <div id="headerInner">
+            <div id="titleOuter">
+              <div id="title"><i><b>Hobby Automation</b></i></div>
+              <div id="subtitle"><i><b>AI Augmented Software</b></i></div>
+            </div>
+            <div id="contentLinks">
+              Overview | Home Automation | AI Assistant | Cloud AI Server | Contact
+            </div>
+          </div>
+          <div id="headerStrip">
+
+          </div>
+        </div>
+
       </div>
     )
   };
